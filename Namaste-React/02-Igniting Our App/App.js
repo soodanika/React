b774e5d96af655e448201d53519113093609784c
sood <soodanika2000@gmail.com>
2024-02-09 14:49:18 +0530

    Chapter 02-Igniting our App

---
+++ Namaste-React/02-Igniting Our App/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+console.log(heading); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading); //take object=>html element
+
+
+const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "Nested - I'm h1"), React.createElement("h2", {}, "Nested - I'm h2")]), React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "Nested - I'm h1"), React.createElement("h2", {}, "Nested - I'm h2")])]);
+root.render(parent);
