72d84397eb6f7e62d6095a1c36b768f2f28afd07
sood <soodanika2000@gmail.com>
2024-02-13 17:07:32 +0530

    Added code for useState hook

--- Namaste-React/05-Lets Get Hooked/src/components/Body.js
+++ Namaste-React/05-Lets Get Hooked/src/components/Body.js
@@ -1,13 +1,32 @@
 import resData from "../utils/mockData";
 import ResturandCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+
+    //Normal JS variable
+    //let resList = resData[0].card.card.gridElements.infoWithStyle.restaurants;
+
+    //Normal State variable
+    let [resList, setListRes] = useState(resData[0].card.card.gridElements.infoWithStyle.restaurants);
+
+    const topRatedRes = () => {
+        let filteredResList = resList.filter((res) => res.info.avgRating > 4.4)
+        setListRes(filteredResList)
+        //console.log(resList)
+    }
+
+    console.log(resList)
+
     return (
         <div className="body">
             <div className="search-container">Search</div>
+            <div className="top-rated-filter">
+                <button className="top-rated-btn" onClick={topRatedRes}>Top Rated Restaurants</button>
+            </div>
             <div className="res-container">
                 {
-                    resData[0].card.card.gridElements.infoWithStyle.restaurants.map((res) => <ResturandCard key={res.info.id} data={res} />)
+                    resList.map((res) => <ResturandCard key={res.info.id} data={res} />)
                 }
             </div>
         </div>
