5c9861518a86061d84c7920707132635714f6041
sood <soodanika2000@gmail.com>
2024-02-13 14:54:36 +0530

    Updated the css file

--- Namaste-React/04-Talk is Cheap Show Me the Code/App.js
+++ Namaste-React/04-Talk is Cheap Show Me the Code/App.js
@@ -1639,7 +1639,7 @@ const ResturandCard = (props) => {
             <img className="res-image" alt="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
                 cloudinaryImageId} />
             <h3>{name}</h3>
-            <h4>{cuisines.join(",")}</h4>
+            <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} stars</h4>
             <h4>{sla.slaString}</h4>
             <h4>{costForTwo}</h4>
