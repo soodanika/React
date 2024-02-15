39cc92cbc3cbd3ff3b6e33beedd2ca7484e43f0c
sood <soodanika2000@gmail.com>
2024-02-15 14:34:50 +0530

    Completed the search functionality.

--- Namaste-React Assignments/Assignment-5/src/components/Body.js
+++ Namaste-React Assignments/Assignment-5/src/components/Body.js
@@ -1,21 +1,31 @@
 import resData from "../utils/mockData";
 import ResturandCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+    let [resList, setResList] = useState(resData[0].card.card.gridElements.infoWithStyle.restaurants);
+    let [filteredList, setFilteredList] = useState(resData[0].card.card.gridElements.infoWithStyle.restaurants)
+    let [searchText, setSearchText] = useState("");
+
 
     const search = (e) => {
-        console.log("clicked", e)
+        let filteredResList = resList.filter((res) => {
+            return res.info.name.toLowerCase().includes(searchText.toLowerCase())
+        })
+        setFilteredList(filteredResList);
     }
 
     return (
         <div className="body">
             <div className="search-container">
-                <input className="search-box" type="text" placeholder="Search..." />
+                <input className="search-box" type="text" placeholder="Search..." value={searchText} onChange={(e) => {
+                    setSearchText(e.target.value);
+                }} />
                 <input className="search-btn" type="submit" value="Go" onClick={search} />
             </div>
             <div className="res-container">
                 {
-                    resData[0].card.card.gridElements.infoWithStyle.restaurants.map((res) => <ResturandCard key={res.info.id} data={res} />)
+                    filteredList.map((res) => <ResturandCard key={res.info.id} data={res} />)
                 }
             </div>
         </div>
